11d1bd16713959af1d685726396b105867cd62b3
權少 <ljqican@163.com>
2019-01-04 22:09:44 +0800

    宏前面全部加LOG_，支持LOG_CRITICAL，支持LOG

--- spdlog_easy.hpp
+++ spdlog_easy.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <spdlog/spdlog.h>
+#include <functional>
 
 namespace spdlog
 {
@@ -76,15 +77,69 @@ namespace spdlog
 		{
 			warn(logger, func, line, "{0}", "");
 		}
+
+		template<typename Arg, typename ...Args>
+		void critical(std::string logger, std::string func, std::size_t line, std::string f, Arg arg, Args&& ...args)
+		{
+			using namespace std;
+			auto log = spdlog::get(logger);
+			if (log == nullptr)
+			{
+				return;
+			}
+			auto fmts = fmt::format("[{0}:{1}] ", func, line) + f;
+			log->critical(fmts.c_str(), arg, forward<Args>(args)...);
+		}
+
+		template<typename T>
+		void critical(std::string logger, std::string func, std::size_t line, T t)
+		{
+			critical(logger, func, line, "{0}", t);
+		}
+
+		inline void critical(std::string logger, std::string func, std::size_t line)
+		{
+			warn(logger, func, line, "{0}", "");
+		}
+
+		template<typename Arg, typename ...Args>
+		void log(spdlog::level::level_enum level, std::string logger, std::string func, std::size_t line, std::string f, Arg arg, Args&& ...args)
+		{
+			using namespace std;
+			auto logg = spdlog::get(logger);
+			if (logg == nullptr)
+			{
+				return;
+			}
+			auto fmts = fmt::format("[{0}:{1}] ", func, line) + f;
+			logg->log(level, fmts.c_str(), arg, forward<Args>(args)...);
+		}
+
+		template<typename T>
+		void log(spdlog::level::level_enum level, std::string logger, std::string func, std::size_t line, T t)
+		{
+			log(level, logger, func, line, "{0}", t);
+		}
+
+		inline void log(spdlog::level::level_enum level, std::string logger, std::string func, std::size_t line)
+		{
+			log(level, logger, func, line, "{0}", "");
+		}
 	}
 }
 
 
-#define INFO(logger, ...) \
+#define LOG_INFO(logger, ...) \
 	spdlog::easy::info(logger, __func__, __LINE__, ##__VA_ARGS__);
 
-#define ERROR(logger, ...) \
+#define LOG_ERROR(logger, ...) \
 	spdlog::easy::error(logger, __func__, __LINE__, ##__VA_ARGS__);
 
-#define WARN(logger, ...) \
+#define LOG_WARN(logger, ...) \
 	spdlog::easy::warn(logger, __func__, __LINE__, ##__VA_ARGS__);
+
+#define LOG_CRITICAL(logger, ...) \
+	spdlog::easy::critical(logger, __func__, __LINE__, ##__VA_ARGS__);
+
+#define LOG(level, logger, ...) \
+	spdlog::easy::log(level, logger, __func__, __LINE__, ##__VA_ARGS__);
