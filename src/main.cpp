af18c06c3c0836157fc920c07ed31d626dffa054
權少 <ljqican@163.com>
2019-07-09 21:09:30 +0800

    添加測試代碼和CMakeLists.txt

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+﻿#include <spdlog_easy.hpp>
+
+int main()
+{
+    spdlog::easy::init();
+    spdlog::set_level(spdlog::level::debug);
+    LOG(info);
+    LOG(warn, "warn");
+    LOG(debug, "{0}", "debug");
+    return 0;
+}
