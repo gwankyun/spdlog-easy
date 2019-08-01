bbbf436f1d237392e17edb2709d52545aeb255f3
權少 <ljqican@163.com>
2019-08-02 00:17:52 +0800

    更新示例代碼

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 ﻿#include <spdlog_easy.hpp>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main()
 {
-    spdlog::easy::init();
+    //spdlog::easy::init();
     spdlog::set_level(spdlog::level::debug);
     LOG(info);
     LOG(warn, "warn");
     LOG(debug, "{0}", "debug");
+    auto logger = spdlog::stderr_color_mt("log");
+    LOG(info, logger);
+    LOG(warn, logger, "warn");
+    LOG(debug, logger, "{0}", "debug");
     return 0;
 }
